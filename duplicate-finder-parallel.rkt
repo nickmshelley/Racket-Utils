c9b1009bf4054ee640527c1e675a5e69a583acbb
Nick Shelley <nickmshelley@gmail.com>
2012-02-17 13:44:07 -0700

    fixed parallel version, but md5 is still pretty slow...

--- duplicate-finder-parallel.rkt
+++ duplicate-finder-parallel.rkt
@@ -1,20 +1,26 @@
 #lang racket
 (require file/md5)
 
+(provide main)
+
 (define (find-duplicates directory)
   (define file-hash (make-hash))
+  (define place-list empty)
   (for ([f (directory-list directory)])
     (define full-path (string-append directory "/" (path->string f)))
     (if (directory-exists? full-path)
         (let ([p (place ch
                    (define directory (place-channel-get ch))
                    (define file-hash (find-duplicates directory))
-                   (place-channel-put (hash->list file-hash)))])
+                   (place-channel-put ch (hash->list file-hash)))])
           (place-channel-put p full-path)
-          (combine-hash! file-hash (place-channel-get p)))
+          (set! place-list (cons p place-list)))
         (add-or-append! file-hash
                         (md5 (file->bytes full-path))
-                        full-path)))
+                        (list full-path))))
+  (for ([p place-list])
+    (define assocs (place-channel-get p))
+    (combine-hash! file-hash assocs))
   file-hash)
 
 (define (print-duplicates file-hash)
@@ -25,14 +31,14 @@
         (printf "\t~a~n" p))
       (newline))))
 
-(define (add-or-append! h k v)
+(define (add-or-append! h k val-list)
   (if (hash-has-key? h k)
-      (hash-set! h k (append (hash-ref h k) (list v)))
-      (hash-set! h k (list v))))
+      (hash-set! h k (append (hash-ref h k) val-list))
+      (hash-set! h k val-list)))
 
 (define (combine-hash! h assocs)
   (for ([p assocs])
-    (add-or-append! h (car assocs) (cdr assocs))))
+    (add-or-append! h (first p) (rest p))))
         
 
 (define (main)
@@ -40,10 +46,9 @@
     (place ch
       (define directory (place-channel-get ch))
       (define file-hash (find-duplicates directory))
-      (place-channel-put (hash->list file-hash))))
+      (place-channel-put ch (hash->list file-hash))))
   
-  (place-channel-put p "/Users/heather/Pictures/Nick's Pictures/random pics")
+  ;(place-channel-put p "/Users/heather/Pictures/Nick's Pictures/random pics")
+  (place-channel-put p "/Users/heather/Pictures/Nick's Pictures/Nick's iPhoto Pictures")
   
   (print-duplicates (make-hash (place-channel-get p))))
-
-(main)
